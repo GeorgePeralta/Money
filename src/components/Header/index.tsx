8bd41049c55dcb51acefbf5bc6e6d14bdc4dd487
George Peralta <george.dias@meta.com.br>
2022-05-25 20:00:07 -0300

    Configurando modal de criação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,11 @@
-import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
-
 
 interface HeaderProps {
     onOpenNewTransactionModal: () => void;
 }
 
-export function Header({onOpenNewTransactionModal}: HeaderProps) {
-
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
     return (
         <Container>
             <Content>
@@ -16,7 +13,6 @@ export function Header({onOpenNewTransactionModal}: HeaderProps) {
                 <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
-
             </Content>
         </Container>
     )
